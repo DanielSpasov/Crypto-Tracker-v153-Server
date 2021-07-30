3e5cb5584a6611048944cfb6304994019b9111c0
Daniel Spasov <danielspasov4@gmail.com>
2021-07-30 10:20:56 +0300

    Registration is now possible.

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,6 +4,8 @@ const userService = require('../services/userService')
 
 
 
+router.post('/register', (req, res) => userService.register(req, res))
+
 router.get('/getOne', (req, res) => userService.getOne(req, res))
 router.get('/getAll', (req, res) => userService.getAll(req, res))
 
--- services/userService.js
+++ services/userService.js
@@ -1,6 +1,34 @@
+const bcrypt = require('bcrypt')
+
 const User = require('../models/User')
 
 
+
+const register = async (req, res) => {
+    try {
+
+        let { email, password, rePassword } = req.body
+        
+        if (!email) return res.status(400).json({ message: 'Email is required' })
+        if (!password) return res.status(400).json({ message: 'Password is required' })
+        if (password.length < 6) return res.status(400).json({ message: 'Password must be at least 6 characters long.' })
+        if (password !== rePassword) return res.status(400).json({ message: 'Passwords doesn\'t match' })
+
+        const username = email.split('@')[0]
+        
+        const existingUser = await User.findOne({ email: email })
+        if (existingUser) return res.status(400).json({ message: 'A user with this email is already registered.' })
+        
+        const salt = await bcrypt.genSalt(Number(process.env.SALT_ROUNDS))
+        const passwordHash = await bcrypt.hash(password, salt)
+
+        const newUser = new User({ email, password: passwordHash, username })
+        await newUser.save()
+        
+        res.json('Successful registration.')
+
+    } catch (err) { res.status(500).json({ message: err.message }) }
+}
 
 const getOne = async (req, res) => {
     let user = await User.findOne({ username: 'ShadyGotRabies' })
@@ -17,4 +45,5 @@ const getAll = async (req, res) => {
 module.exports = {
     getOne,
     getAll,
+    register
 }
