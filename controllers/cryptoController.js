f294ec08a071c25aefcff8c5daf6961ee514fc27
Daniel Spasov <danielspasov4@gmail.com>
2021-08-03 10:48:38 +0300

    Added edit watchlist endpoints.

--- controllers/cryptoController.js
+++ controllers/cryptoController.js
@@ -6,8 +6,9 @@ const cryptoService = require('../services/cryptoService')
 
 router.get('/getOne', (req, res) => cryptoService.getOne(req, res))
 router.get('/getTop100', (req, res) => cryptoService.getTop100(req, res))
-router.get('/addToWatchlist', (req, res) => cryptoService.addToWatchlist(req, res))
-router.get('/removeFromWatchlist', (req, res) => cryptoService.removeFromWatchlist(req, res))
+router.get('/getWatchlistCryptos', (req, res) => cryptoService.getWatchlistCryptos(req, res))
+
+router.post('/editWatchlist', (req, res) => cryptoService.editWatchlist(req, res))
 
 
 
--- services/cryptoService.js
+++ services/cryptoService.js
@@ -1,5 +1,7 @@
 const axios = require('axios')
 
+const User = require('../models/User')
+
 
 
 const getOne = async (req, res) => {
@@ -16,12 +18,42 @@ const getTop100 = async (req, res) => {
     res.json(data.data)
 }
 
-const addToWatchlist = async (req, res) => {
-    res.json({ 'In Add To Watchlist': req.query.crypto })
+const editWatchlist = async (req, res) => {
+    try {
+
+        let user = await User.findById(req.body.userID)
+
+        if (user.watchlist.includes(req.body.crypto)) {
+            user.watchlist.splice(user.watchlist.indexOf(req.body.crypto), 1)
+            user.save()
+            res.json({ email: user.email, username: user.username, id: user._id, watchlist: user.watchlist })
+        } else {
+            user.watchlist.push(req.body.crypto)
+            user.save()
+            res.json({ email: user.email, username: user.username, id: user._id, watchlist: user.watchlist })
+        }
+
+    } catch (err) { res.status(500).json({ message: err.message }) }
 }
 
-const removeFromWatchlist = async (req, res) => {
-    res.json({ 'In Remove From Watchlist': req.query.crypto })
+const getWatchlistCryptos = async (req, res) => {
+    try {
+
+        let user = await User.findById(req.query.userID)
+
+        let watchlistData = await axios
+            .get(`https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=${user.watchlist.join(',')}`, {
+                headers: { 'X-CMC_PRO_API_KEY': '9022ea0d-dc6b-4fb8-bb12-30c8ff5dd270' }
+            })
+
+        let output = []
+        for (let i of user.watchlist) {
+            output.push(watchlistData.data.data[i])
+        }
+
+        res.json(output)
+
+    } catch (err) { res.status(500).json({ message: err.message }) }
 }
 
 
@@ -29,6 +61,6 @@ const removeFromWatchlist = async (req, res) => {
 module.exports = {
     getOne,
     getTop100,
-    addToWatchlist,
-    removeFromWatchlist
+    editWatchlist,
+    getWatchlistCryptos
 }
