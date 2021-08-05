2d3793b2dc3f0db532fbd13f4a668fa7f9fd7898
Daniel Spasov <danielspasov4@gmail.com>
2021-08-05 11:33:52 +0300

    Made sending data in a cleaner way

--- services/cryptoService.js
+++ services/cryptoService.js
@@ -5,17 +5,17 @@ const User = require('../models/User')
 
 
 const getOne = async (req, res) => {
-    let data = await axios.get(`https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=${req.query.crypto}`, {
+    let cryptoData = await axios.get(`https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=${req.query.crypto}`, {
         headers: { 'X-CMC_PRO_API_KEY': '9022ea0d-dc6b-4fb8-bb12-30c8ff5dd270' }
     })
-    res.json(data.data)
+    res.json(cryptoData.data.data)
 }
 
 const getTop100 = async (req, res) => {
-    let data = await axios.get(`https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest`, {
+    let cryptoData = await axios.get(`https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest`, {
         headers: { 'X-CMC_PRO_API_KEY': '9022ea0d-dc6b-4fb8-bb12-30c8ff5dd270' }
     })
-    res.json(data.data)
+    res.json(cryptoData.data.data)
 }
 
 const editWatchlist = async (req, res) => {
